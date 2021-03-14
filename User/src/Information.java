ac54003b2844f5c7bcb5506f0e0efa6716f3f784
DELL <DELL@DESKTOP-IF2PEG5>
2021-03-14 09:02:17 +0300

    First C

---
+++ User/src/Information.java
@@ -0,0 +1,21 @@
+
+public class Information {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	MyInformation A =new MyInformation();
+	A.setEmail("43***@hotmail.com");
+	A.setPassword(52918273);
+	A.setUsername("mshaman");
+	A.getEmail();
+	A.getPassword();
+A.getUsername();
+System.out.println("there is my information ");
+System.out.println(A.getUsername());
+System.out.println(A.getEmail());
+System.out.println(A.getPassword());
+
+
+	}
+
+}
---
+++ User/src/MyInformation.java
@@ -0,0 +1,26 @@
+
+ class MyInformation {
+	private String email;
+	private int password;
+	private String username;
+	
+	public  String getEmail () {
+		return email;
+	}
+public int getPassword() {
+return password;
+}
+public String getUsername() {
+	return username;
+}
+public void setEmail(String E) {
+	email=E;
+}
+public void setPassword(int Pass) {
+	password=Pass;
+}
+public void setUsername(String User) {
+	username=User;
+}
+
+}
